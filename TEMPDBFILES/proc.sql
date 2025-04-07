f5b31fc8de5d51988d2021748ca848e5af5d31eb
Hvogu <92995572+Hvogu@users.noreply.github.com>
2025-04-07 21:20:03 +0200

    Update proc.sql

--- TEMPDBFILES/proc.sql
+++ TEMPDBFILES/proc.sql
@@ -21,58 +21,45 @@ BEGIN
             ELSE 1
 		END AS temp2
 	INTO localVar, localVar2
-	FROM employees
+	FROM orders
     where id = 1;
         
-	insert into employees VALUES (1,first_name,0,0,0,'2024-05-05',0,0) ON DUPLICATE KEY UPDATE salary = localVar, department_id = localVar2;
+	insert into employees VALUES (1,first_name,0,0,0,'2024-05-05',0,0) ON DUPLICATE KEY UPDATE id = 2, salary = localVar, department_id = localVar2;
 END |
 DELIMITER ;
 
 DROP PROCEDURE IF EXISTS GetNthPrimaryKey;
 DELIMITER |
-CREATE PROCEDURE GetNthPrimaryKey(IN n INT, IN tableName VARCHAR(64), OUT primaryKeyValue INT)
+CREATE PROCEDURE GetNthPrimaryKey(IN n Integer, IN tableName VARCHAR(64), OUT primaryKeyValue INT)
 BEGIN
-    DECLARE qry1 TEXT;
-    DECLARE qry2 TEXT;
+
     DECLARE primaryKeyCol VARCHAR(64);
     DECLARE result INT;
 
-    SET n = n - 1; -- 0 indexed
-    prepare stmt from 'SELECT column_name FROM information_schema.KEY_COLUMN_USAGE WHERE table_schema = DATABASE() AND table_name = ? AND constraint_name = \'PRIMARY\' LIMIT 1';
-set @table_name = 'employees';
-execute stmt using @table_name;
-
-SET primary_key_column = temp_column;
-deallocate prepare stmt;
--- 
     -- Build query to find the name of the primary key
-    SET qry1 = CONCAT(
+    SET @qry1 = CONCAT(
         'SELECT column_name FROM information_schema.KEY_COLUMN_USAGE ',
-        'WHERE table_schema = DATABASE() AND table_name = \'', tableName, 
-        '\' AND constraint_name = \'PRIMARY\' LIMIT 1'
+        'WHERE table_schema = DATABASE() AND table_name = \'', tableName, '\' ',
+        'AND constraint_name = \'PRIMARY\' LIMIT 1'
     );
     
-    -- Execute query to get name of primary key into var
-    PREPARE exe FROM qry1;
-    EXECUTE exe INTO @primaryKeyCol;
-    DEALLOCATE PREPARE exe;
-    
-    -- Get value of primaryKeyCol into local var
-    SET primaryKeyCol = @primaryKeyCol;
-    
+    PREPARE stmt FROM @qry1;
+    EXECUTE stmt INTO primaryKeyCol;
+    DEALLOCATE PREPARE stmt;
+
     -- Build query to get the Nth primary key value
-    SET qry2 = CONCAT(
+    SET @qry2 = CONCAT(
         'SELECT ', primaryKeyCol, ' FROM ', tableName, 
         ' ORDER BY ', primaryKeyCol, ' LIMIT 1 OFFSET ', n
     );
     
     -- Execute query to get nth key and store result in @result
-    PREPARE exe FROM qry2; 
-    EXECUTE exe INTO @result; 
+    PREPARE exe FROM @qry2; 
+    EXECUTE exe INTO result; 
     DEALLOCATE PREPARE exe;
     
     -- Return nth key
-    SET primaryKeyValue = @result;
+    SET primaryKeyValue = result;
 END |
 DELIMITER ;
 
@@ -90,7 +77,6 @@ BEGIN
     
     -- query to get the num of rows of a table
     SET @qry = CONCAT('SELECT COUNT(*) INTO @rowCount FROM ', tableName, ';');
-	set @tableName = tableName;
     
     -- execute query
     PREPARE exe FROM @qry;
@@ -101,10 +87,12 @@ BEGIN
     
     select rowCount as numOfRows;
     
-    -- while counter <= rowCount do
-		-- call specific proc
-        -- set counter = counter + 1;
-	-- END WHILE;
+    while counter <= rowCount do
+		
+        set counter = counter + 1;
+        select counter as count;
+	END WHILE;
+
 
 END |
 DELIMITER ;
